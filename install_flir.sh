7aa06da3cf985f55538791953da5f350ea616818
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2023-04-21 20:34:28 +0900

    Update install_flir.sh

--- install_flir.sh
+++ install_flir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-wget https://flir.netx.net/file/asset/52327/original/attachment -O spinnaker-amd64-pkg.tar.gz
-tar -zxvf spinnaker-amd64-pkg.tar.gz 
+wget https://flir.netx.net/file/asset/52327/original/attachment -O spinnaker-3.0.0.118-amd64-pkg.tar.gz
+tar -zxvf spinnaker-3.0.0.118-amd64-pkg.tar.gz
 cd spinnaker-3.0.0.118-amd64
 ./install_spinnaker.sh
