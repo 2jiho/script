3dab62d35ee07b55c0702bdcd2ffa5718d6d0eb0
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2023-04-25 00:56:58 +0900

    Delete install_flir.sh

--- install_flir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# https://www.flirkorea.com/support-center/iis/machine-vision/downloads/spinnaker-sdk-download/spinnaker-sdk--download-files/
-cd /tmp
-wget https://flir.netx.net/file/asset/52327/original/attachment -O spinnaker-3.0.0.118-amd64-pkg.tar.gz
-tar -zxvf spinnaker-3.0.0.118-amd64-pkg.tar.gz
-cd spinnaker-3.0.0.118-amd64
-./install_spinnaker.sh
