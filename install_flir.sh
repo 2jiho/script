88396b1ae75c5af4befbbf8e2f02853dd3902301
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2023-04-26 13:24:26 +0900

    Update install_flir.sh

--- install_flir.sh
+++ install_flir.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # https://www.flirkorea.com/support-center/iis/machine-vision/downloads/spinnaker-sdk-download/spinnaker-sdk--download-files/
+
+sudo apt-get install -y \
+    libswscale-dev \
+    libavcodec-dev \
+    libavformat-dev
+     
+     
 cd /tmp
 wget https://flir.netx.net/file/asset/52327/original/attachment -O spinnaker-3.0.0.118-amd64-pkg.tar.gz
 tar -zxvf spinnaker-3.0.0.118-amd64-pkg.tar.gz
